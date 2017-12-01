7472531ff059abb9ac0804d656ae818e029608ca
Péter Szrnka <szrnka.peter@gmail.com>
2017-12-01 17:12:40 +0100

    Create YUICSSCompressorTest.java

---
+++ src/test/java/hu/szrnkapeter/cssjsminifier/compressor/css/YUICSSCompressorTest.java
@@ -0,0 +1,27 @@
+package hu.szrnkapeter.cssjsminifier.compressor.css;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class YUICSSCompressorTest {
+
+	@Test
+	public void test() throws Exception {
+		final YUICSSCompressor compressor = new YUICSSCompressor();
+		final File tempFile = File.createTempFile("prefix", "suffix");
+		final BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile));
+		final StringBuilder sb = new StringBuilder();
+		sb.append(".custom-style {\r\ncolor:fff;\r\n}\r\n");
+		bw.write(sb.toString());
+		bw.close();
+		final String result = compressor.compress(tempFile.getAbsolutePath());
+
+		tempFile.deleteOnExit();
+
+		Assert.assertEquals("Wrong result!", ".custom-style{color:fff;}", result);
+	}
+}
