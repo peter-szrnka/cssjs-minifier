b28ddb14fc8a515f7e534eeaa2cf7f02882e7bff
Péter Szrnka <szrnka.peter@gmail.com>
2017-12-01 17:14:12 +0100

    Create MainTest.java

---
+++ src/test/java/hu/szrnkapeter/cssjsminifier/MainTest.java
@@ -0,0 +1,32 @@
+package hu.szrnkapeter.cssjsminifier;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class MainTest {
+
+	@Test
+	@Ignore
+	public void test() throws Exception {
+		final String[] strings = new String[0];
+
+		final Path tempDir = Files.createTempDirectory("js");
+
+		final Path tempFile = Files.createTempFile(tempDir, "prefix", "js");
+		final BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile.toFile()));
+		final StringBuilder sb = new StringBuilder();
+		sb.append("var doit = function(str) {\r\nalert (str);\r\n};");
+		bw.write(sb.toString());
+		bw.close();
+
+		Main.main(strings);
+
+		Files.delete(tempFile);
+		Files.deleteIfExists(tempDir);
+	}
+}
