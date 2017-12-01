4bb972ee8f3b90de3d9ecd395104a1120b2a02d6
Péter Szrnka <szrnka.peter@gmail.com>
2017-12-01 17:13:00 +0100

    Create GoogleClosureCompilerJSCompressorTest.java

---
+++ src/test/java/hu/szrnkapeter/cssjsminifier/compressor/js/GoogleClosureCompilerJSCompressorTest.java
@@ -0,0 +1,45 @@
+package hu.szrnkapeter.cssjsminifier.compressor.js;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import hu.szrnkapeter.cssjsminifier.util.JSCompileType;
+
+public class GoogleClosureCompilerJSCompressorTest {
+
+	@Test
+	public void test_normal_simple() throws Exception {
+		final GoogleClosureCompilerJSCompressor compressor = new GoogleClosureCompilerJSCompressor();
+		final File tempFile = File.createTempFile("prefix", "suffix");
+		final BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile));
+		final StringBuilder sb = new StringBuilder();
+		sb.append("var doit = function(str) {\r\nalert (str);\r\n};");
+		bw.write(sb.toString());
+		bw.close();
+		final String result = compressor.compress(tempFile.getAbsolutePath(), JSCompileType.SIMPLE);
+
+		tempFile.deleteOnExit();
+
+		Assert.assertEquals("Wrong result!", "var doit=function(a){alert(a)};", result);
+	}
+
+	@Test
+	public void test_normal_whitespace() throws Exception {
+		final GoogleClosureCompilerJSCompressor compressor = new GoogleClosureCompilerJSCompressor();
+		final File tempFile = File.createTempFile("prefix", "suffix");
+		final BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile));
+		final StringBuilder sb = new StringBuilder();
+		sb.append("var doit = function(str) {\r\nalert (str);\r\n};");
+		bw.write(sb.toString());
+		bw.close();
+		final String result = compressor.compress(tempFile.getAbsolutePath(), JSCompileType.WHITESPACE);
+
+		tempFile.deleteOnExit();
+
+		Assert.assertEquals("Wrong result!", "var doit=function(str){alert(str)};", result);
+	}
+}
